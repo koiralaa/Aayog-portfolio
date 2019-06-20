be9983086676ed6076e930ceee63a5eb94047852
Bishesh Acharya <acharya.bises@outlook.com>
2019-06-20 18:16:57 -0400

    using p5.js library in angular way

--- my-portfolio/src/app/sketch-area/sketch-area.component.ts
+++ my-portfolio/src/app/sketch-area/sketch-area.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { Sketch } from '../sketch';
 import { SKETCHES } from '../mock-sketches'
+import * as p5 from 'p5'
 
 @Component({
   selector: 'app-sketch-area',
@@ -8,12 +9,36 @@ import { SKETCHES } from '../mock-sketches'
   styleUrls: ['./sketch-area.component.css']
 })
 export class SketchAreaComponent implements OnInit {
-  
-  sketches = SKETCHES;
+
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+
+  ps5: any;
   
   constructor() { }
 
   ngOnInit() {
+    this.createcanvas();
   }
 
+  createcanvas(): any {
+    this.ps5 = new p5(this.sketch, 'c1');
+    this.ps5 = new p5(this.sketch, 'c2');
+  }
+
+  private sketch(p: any) {
+    let xSpeed = 1;
+    let ySpeed = 1;
+    p.setup = () => {
+      p.createCanvas(480, 320);
+    };
+
+    p.draw = () => {
+      p.background(0);
+      p.ellipse(p.width / 2, p.height / 2, 16, 16);
+      p.width += xSpeed ;
+      p.height += ySpeed;
+      if (p.width > (480 * 2 - 8) || p.width < 8) xSpeed *= -1;
+      if (p.height > (320 * 2 - 8) || p.height < 8) ySpeed *= -1;
+    };
+  }
 }
