e34acccb87054757eb3be47ef89a4f6b89a9a768
Bishesh Acharya <acharya.bises@outlook.com>
2019-06-23 11:44:28 -0400

    removed unused import

--- my-portfolio/src/app/sketch-area/sketch-area.component.ts
+++ my-portfolio/src/app/sketch-area/sketch-area.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as p5 from 'p5'
 
 @Component({
